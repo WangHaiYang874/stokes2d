b0c03a73e6b252fce197c77d69493fa13954e561
Haiyang Wang <hw1927@nyu.edu>
2022-08-03 16:02:27 -0400

    refactoring

--- src/solver.py
+++ src/solver.py
@@ -1,50 +1,30 @@
 import numpy as np
 from scipy.sparse.linalg import gmres
 import numbers
+from basic_spec import *
 
-def v2h(u,v):
-    '''
-    transform velocity into the complex numbers. 
-    '''
-    return -v + 1j*u
-
-def h2v(h):
-    '''
-    transform the complex representation of velocity into velocity. 
-    '''
-    return np.array([h.imag, -h.real]).T
 
 class stokes2d:
-    def __init__(self, geometry, u, v, rho=1, nu=1,gmres_tol=1e-12):
-        '''
-        h = -v + 1j*u
-        '''
+    def __init__(self, geometry, gmres_tol=5e-13):
+        
         self.geometry = geometry
-        self.u = u
-        self.v = v
-        self.h2 = u
-        self.h1 = -v
-        self.rho = rho
-        self.nu = nu
         self.gmres_tol = gmres_tol
-        self.solve()
 
-    def solve(self):
+    def build_A(self):
+        '''this builds the matrix for the Nystorm discretization'''
         
         # compute the kernels
-                
         _, da, t, dt_da, k = self.geometry.get_data()
         dt = t[:, np.newaxis] - t[np.newaxis, :]
         d = dt_da[np.newaxis, :]
         da_ = da[np.newaxis, :]
-
 
         # this ignore the error for computing the diagonal elements with 0/0 error
         with np.errstate(divide='ignore', invalid='ignore'):
             K1 = -da_ * np.imag(d/dt) / np.pi
             K2 = -da_ * (-d/np.conjugate(dt) + np.conjugate(d)
                          * dt/(np.conjugate(dt**2))) / (2j*np.pi)
-
+            
         # now we need to fill the diagonal elements
         d = dt_da
         K1_diagonal = k*np.abs(d)*da/(2*np.pi)
@@ -55,27 +35,39 @@ class stokes2d:
         # building the equation for gmres
         
         n = len(self.geometry.a)
+        self.n = n
+        
         A = np.zeros((2*n, 2*n))
         A[:n, :n] = np.identity(n) + (K1+K2).real
         A[:n, n:] = (-K1+K2).imag
         A[n:, :n] = (K1+K2).imag
         A[n:, n:] = np.identity(n) + (K1-K2).real
-        rhs = np.concatenate((self.h1, self.h2))
+        
+        self.A = A
+        
+    def solve(self,U):
+
+        H = U2H(U)
+        
+        rhs = np.concatenate((H.real, H.imag))
+        
         print('gmres starts solving the Nystorm, please wait...')
-        omega, _ = gmres(A, rhs, tol=self.gmres_tol, atol=0)
+        
+        omega, _ = gmres(self.A, rhs, tol=self.gmres_tol, atol=0)
+        
         if _ == 0:
             print('gmres converged')
         else:
             print('gmres did not converge')
             print('gmres error:', _)
-        omega = omega[:n] + 1j*omega[n:]
-        self.omega = omega
+        omega = omega[:self.n] + 1j*omega[self.n:]
+        
+        return omega
 
-    def compute_velocity(self, z):
-
+    def compute_velocity(self, z, omega):
+        
         t = self.geometry.get_t()
         dt = self.geometry.get_dt_da()*self.geometry.da
-        omega = self.omega
 
         if isinstance(z, numbers.Number):
             t_minus_z = t - z
@@ -89,7 +81,10 @@ class stokes2d:
                 - np.sum(np.conjugate(t)*omega*dt/t_minus_z_sq))
 
         else:
-            assert isinstance(z, np.ndarray) and z.ndim == 1
+            assert isinstance(z, np.ndarray)
+            shape = z.shape
+            z = z.flatten()
+            
             t_minus_z = t[np.newaxis, :] - z[:, np.newaxis]
             t_minus_z_sq = t_minus_z**2
 
@@ -104,15 +99,15 @@ class stokes2d:
                 - np.sum((np.conjugate(t)*omega*dt)[np.newaxis, :]/t_minus_z_sq, axis=1))
 
         ret = phi + z*np.conjugate(d_phi) + np.conjugate(psi)
-        # ret.imag = u
-        # ret.real = -v
-        return h2v(ret)
+        
+        ret = ret.reshape(shape)
+        
+        return H2U(ret)
 
-    def compute_grad_pressure(self, z):
+    def compute_grad_pressure(self, z, omega):
 
         t = self.geometry.get_t()
         dt = self.geometry.get_dt_da()*self.geometry.da
-        omega = self.omega
 
         if isinstance(z, numbers.Number):
             dd_phi = np.sum(omega*dt/(t-z)**3)/(1j*np.pi)
@@ -123,11 +118,8 @@ class stokes2d:
             dd_phi = np.sum((omega*dt)[np.newaxis, :] /
                             t_minus_z_cubic, axis=1)/(1j*np.pi)
 
-        grad_p = -4*self.rho*self.nu*(dd_phi.imag + 1j*dd_phi.real)
+        grad_p = -4*np.array((dd_phi.imag,dd_phi.real)).T
         return grad_p
     
-    def build_pressure_drop(self):
-        pass
-    def build_velocity_field(self):
-        pass
-    
+class stokes2dGlobal:
+    pass
