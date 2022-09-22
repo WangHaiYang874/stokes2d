fd29bcf6855b3eeaff594a0770d1b49e5829ff47
Haiyang Wang <hw1927@nyu.edu>
2022-09-22 11:17:08 -0400

    improving numerical accuracy of corner

--- src/math_functions.py
+++ src/math_functions.py
@@ -1,12 +1,16 @@
 import numbers
-from scipy.integrate import quad
+from scipy.integrate import quad, IntegrationWarning
 import numpy as np
 
 __all__ = ['_psi', '_Psi', '_int_Psi', '_d_psi', 
-           '_bump', '_d_bump', '_d_d_bump', '_normalized_bump', 
+           '_bump', '_d_bump', '_d_d_bump',
            'convoluted_abs', 'd_convoluted_abs', 'dd_convoluted_abs', 
            '_int_Psi_normalizer']
 
+ERR = 1e-17
+
+import warnings
+warnings.simplefilter("ignore",IntegrationWarning)
 
 def _psi(a: np.ndarray) -> np.ndarray:
 
@@ -17,9 +21,11 @@ def _psi(a: np.ndarray) -> np.ndarray:
     ret[abs(a) >= 1] = 0
     ret[np.isnan(ret)] = 0
     ret[ret == np.inf] = 0
+
     return ret
 
 
+# noinspection PyPep8Naming
 def _Psi(a):
 
     if isinstance(a, np.ndarray):    
@@ -31,7 +37,9 @@ def _Psi(a):
         if np.any(nan_mask):
             ret[nan_mask & a > 0.9] = 1/8
             ret[nan_mask & a < -0.9] = -1/8
+        assert np.sum(np.isnan(ret)) == 0
         return ret
+
     if isinstance(a,numbers.Number):
         if a >= 1:
             return 1/8
@@ -40,14 +48,16 @@ def _Psi(a):
         return -np.tanh(-2*a/(1-a**2))/8
     
 
-_int_Psi_normalizer = 1/quad(_Psi,0,1,epsabs=1e-14,epsrel=1e-14)[0]
+_int_Psi_normalizer = 1/quad(_Psi,0,1,epsabs=ERR,epsrel=ERR)[0]
 
+
+# noinspection PyPep8Naming
 def _int_Psi(a):
-    '''the number 128 in the expression below is chosen by numerical experiment'''
+    """the number 128 in the expression below is chosen by numerical experiment"""
     if isinstance(a, np.ndarray):
-        return np.array([quad(_Psi, -1, b, epsabs=1e-14,epsrel=1e-14)[0] for b in a])
+        return np.array([quad(_Psi, -1, b, epsabs=ERR,epsrel=ERR)[0] for b in a])
     if isinstance(a, numbers.Number):
-        return quad(_Psi, -1, a, epsabs=1e-14,epsrel=1e-14)[0]
+        return quad(_Psi, -1, a, epsabs=ERR,epsrel=ERR)[0]
     else:
         print('input is not a number or a numpy array')
         assert 0
@@ -92,27 +102,39 @@ def _d_d_bump(a: np.ndarray) -> np.ndarray:
     return ret
 
 
-bump_def_int = quad(_bump, -1, 1, epsabs=1e-14,epsrel=1e-14)[0]
+bump_def_int = quad(_bump, -1, 1, epsabs=ERR, epsrel=ERR)[0]
 
-def _normalized_bump(a):
-    return _bump(a)/bump_def_int
+# def convoluted_abs(x):
+#     if np.abs(x) >= 1:
+#         return np.abs(x)
+#
+#     def b(y): return _bump(y)*np.abs(x-y)
+#     return quad(b, -1, 1, epsabs=ERR, epsrel=ERR, full_output=1)[0]/bump_def_int
+# this function has too much numerical error. It is replaced with
 
 def convoluted_abs(x):
     if np.abs(x) >= 1:
-        return np.abs(x)
+        return 0
+    x = np.abs(x)
 
-    def b(y): return _normalized_bump(y)*np.abs(x-y)
-    return quad(b, -1, 1, epsabs=1e-15, epsrel=1e-15, full_output=1)[0]
+    a = quad(_bump,
+               0,x,
+               epsabs=ERR, epsrel=ERR)[0]
+    b = quad(lambda y: y*_bump(y),
+               x,1,
+               epsabs=ERR, epsrel=ERR)[0]
+    return  2*(x*a + b)/bump_def_int
+
 
 def d_convoluted_abs(x):
     if np.abs(x) >= 1:
         return np.sign(x)
 
     def b(y): return _bump(y)
-    return +quad(b, -1, x, epsabs=1e-15, epsrel=1e-15, full_output=1)[0]\
-        - quad(b, x, 1, epsabs=1e-15, epsrel=1e-15, full_output=1)[0]
+    return +quad(b, -1, x, epsabs=ERR, epsrel=ERR, full_output=1)[0]\
+        - quad(b, x, 1, epsabs=ERR, epsrel=ERR, full_output=1)[0]
 
 def dd_convoluted_abs(x):
     if np.abs(x) >= 1:
         return 0
-    return 2*_normalized_bump(x)
+    return 2*_bump(x)/bump_def_int
