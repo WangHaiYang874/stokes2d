af695ace14088d23c3cf61e578e76fe1e5e41fd6
Haiyang Wang <hw1927@nyu.edu>
2022-10-01 16:04:18 -0400

    on-going

--- src/GPipe.py
+++ src/GPipe.py
@@ -15,7 +15,7 @@ class Let:
     def match(self, other: 'Let'):
         return max(
                 np.linalg.norm(self.pos - other.pos),
-                np.abs((self.dir-other.dir) % (2*np.pi)), 
+                np.abs((self.dir-other.dir) % (2*np.pi) - np.pi), 
                 np.abs(self.dia - other.dia)) \
             < 1e-10
 
@@ -29,7 +29,17 @@ class BoundaryLet(Let):
 class GraphComponent:
     
     lets: List[Let]
+    def __init__(self) -> None:
+        pass
+
+    def fluxes_at_lets(self,fluxes):
+        assert len(fluxes) == len(self.lets)-1
+        return np.concatenate([ [-np.sum(fluxes)], fluxes])
     
+class PipeGraphComponent(GraphComponent):
+    lets: List[Let]
+    boundary: np.ndarray
+    pressure_drops: np.ndarray
     def __init__(self,p:Pipe,shift_x=0,shift_y=0) -> None:
         
         self.lets = []
@@ -38,21 +48,14 @@ class GraphComponent:
         for l  in p.lets:
             c = p.curves[l]
             self.lets.append(Let(c.matching_pt + shift, c.dir, c.diameter))
-        # self.boundary = p.boundary.copy() + shift
-        # self.pressure_drops = p.pressure_drops
-        
-    def fluxes_at_lets(fluxes):
-        # return np.concatenate([ [-np.sum(fluxes)], fluxes])
-        pass
-    
-    def pressure_drops_at_lets(self, fluxes):
-        # return np.dot(fluxes, self.pressure_drops)
-        pass
-    
-class PipeGraphComponent(GraphComponent):
-    
-    def __init__(self,p:Pipe,shift_x=0,shift_y=0) -> None:
-        super().__init__(p,shift_x,shift_y)
         self.boundary = p.boundary.copy() + shift
         self.pressure_drops = p.pressure_drops
+        
+    def pressure_drops_at_lets(self, fluxes):
+        return np.dot(fluxes, self.pressure_drops)
     
+class BoundaryGraphComponent(GraphComponent):
+    def __init__(self,lets:List[BoundaryLet]) -> None:
+        self.lets = lets
+    def fluxes_at_lets(self):
+        return np.array([l.flux for l in self.lets])
